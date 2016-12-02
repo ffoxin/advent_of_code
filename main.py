87e85441526c3042c06fab1c585062639f0e4af1
Vital Kolas <vital.kolas@gmail.com>
2016-12-02 21:07:58 +0300

    Day 2

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from tasks import day1
+from tasks import day2
+
+
 def main():
-    pass
+    day1.puzzle1()
+    day1.puzzle2()
+
+    day2.puzzle1()
+    day2.puzzle2()
+
 
 if __name__ == '__main__':
     main()
