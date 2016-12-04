c617ad700f536add73e56b65a55bd9956a021d27
Vital Kolas <vital.kolas@gmail.com>
2016-12-04 12:29:51 +0300

    Day 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tasks import day1
 from tasks import day2
+from tasks import day3
 
 
 def main():
@@ -8,6 +9,9 @@ def main():
 
     day2.puzzle1()
     day2.puzzle2()
+
+    day3.puzzle1()
+    day3.puzzle2()
 
 
 if __name__ == '__main__':
