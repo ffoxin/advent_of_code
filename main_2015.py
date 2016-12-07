7366e643b53d44963ae4a499ddccb733323891e8
Vital Kolas <vital.kolas@gmail.com>
2016-12-08 00:53:11 +0300

    AoC 2015 - Day 7

--- main_2015.py
+++ main_2015.py
@@ -4,7 +4,7 @@ from importlib import import_module
 
 def main():
     for item in sorted(os.listdir('tasks/2015')):
-        if item.startswith('day') and not item.startswith('day4'):
+        if item.startswith('day') and not item.startswith('day4') and not item.startswith('day6'):
             module, _ = os.path.splitext(item)
             module_name = 'tasks.2015.{}'.format(module)
             module = import_module(module_name)
