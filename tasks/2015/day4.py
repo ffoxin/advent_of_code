b01648d3053706fc9b3b628c2b0cec2124e0337d
Vital Kolas <vital.kolas@gmail.com>
2016-12-07 20:34:10 +0300

    AoC 2015 - Day 4

---
+++ tasks/2015/day4.py
@@ -0,0 +1,31 @@
+import hashlib
+
+
+def puzzle1():
+    secret = 'iwrupvqb'
+    index = 1
+    md5 = hashlib.md5()
+    while True:
+        key = '{}{}'.format(secret, index)
+        hasher = md5.copy()
+        hasher.update(key.encode())
+        if hasher.hexdigest().startswith('00000'):
+            print(index)
+            return
+        index += 1
+
+
+def puzzle2():
+    secret = 'iwrupvqb'
+    index = 1
+    md5 = hashlib.md5()
+    while True:
+        key = '{}{}'.format(secret, index)
+        hasher = md5.copy()
+        hasher.update(key.encode())
+        if hasher.hexdigest().startswith('000000'):
+            print(index)
+            return
+        index += 1
+
+
