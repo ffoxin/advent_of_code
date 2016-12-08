063b139006400d756d5f1d65ec66d4807e465004
Vital Kolas <vital.kolas@gmail.com>
2016-12-08 20:43:45 +0300

    Fixed AoC 2015 launcher

--- main_2015.py
+++ main_2015.py
@@ -3,9 +3,10 @@ from importlib import import_module
 
 
 def main():
-    for item in sorted(os.listdir('tasks/2015')):
-        if item.startswith('day') and not item.startswith('day4') and not item.startswith('day6'):
-            module, _ = os.path.splitext(item)
+    for item in sorted(os.listdir('tasks/2015'),
+                       key=lambda name: int(name[3:name.index('.')]) if name[:3] == 'day' else 0):
+        module, _ = os.path.splitext(item)
+        if module.startswith('day') and module not in ['day4', 'day6', 'day9', 'day10', 'day11']:
             module_name = 'tasks.2015.{}'.format(module)
             module = import_module(module_name)
 
