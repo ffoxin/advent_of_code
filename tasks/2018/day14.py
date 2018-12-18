86f0e19cccf4bfcbd89d9380ca9865d22f476e4f
Vital Kolas <vital.kolas@gmail.com>
2018-12-18 16:33:06 +0300

    AoC 2018 - Day 14

---
+++ tasks/2018/day14.py
@@ -0,0 +1,63 @@
+from collections import deque
+
+from main import data_path
+
+DATA = data_path(__file__)
+
+
+def puzzle1():
+    number = 920831
+
+    scoreboard = [3, 7]
+    elf1 = 0
+    elf2 = 1
+
+    def get_div(index, start=True):
+        if index == elf1:
+            result = '(' if start else ')'
+        elif index == elf2:
+            result = '[' if start else ']'
+        else:
+            result = ' '
+
+        return result
+
+    def print_board():
+        print(''.join(
+            '{}{}{}'.format(get_div(i), e, get_div(i, False)) for i, e in enumerate(scoreboard)
+        ))
+
+    print_board()
+    while True:
+        if len(scoreboard) >= 10 + number:
+            break
+
+        new_recipe = scoreboard[elf1] + scoreboard[elf2]
+
+        if new_recipe < 10:
+            scoreboard.append(new_recipe)
+        else:
+            scoreboard.append(new_recipe // 10)
+            scoreboard.append(new_recipe % 10)
+
+        elf1 = (elf1 + 1 + scoreboard[elf1]) % len(scoreboard)
+        elf2 = (elf2 + 1 + scoreboard[elf2]) % len(scoreboard)
+
+    # print_board()
+    print(''.join(map(str, scoreboard[-10:])))
+
+
+def puzzle2():
+    target = '920831'
+
+    scoreboard = '37'
+
+    elf1 = 0
+    elf2 = 1
+    while target not in scoreboard[-7:]:
+        scoreboard += str(int(scoreboard[elf1]) + int(scoreboard[elf2]))
+        elf1 = (elf1 + int(scoreboard[elf1]) + 1) % len(scoreboard)
+        elf2 = (elf2 + int(scoreboard[elf2]) + 1) % len(scoreboard)
+
+    # print_board()
+    print(scoreboard.index(target))
