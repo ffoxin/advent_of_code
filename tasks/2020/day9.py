5d9bca20b7e8d125a1c0554fe39054ce0c860c07
Vitaly Kolos <v_kolos@wargaming.net>
2020-12-09 20:27:16 +0300

    AoC 2020 - Day 9

---
+++ tasks/2020/day9.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+from pathlib import Path
+
+DATA = (Path(__file__).parent / 'data' / 'day9.txt').read_text()
+
+
+def puzzle1():
+    entries = [int(i) for i in DATA.split('\n') if i]
+
+    preamble = 25
+    for i in range(len(entries)):
+        if i < preamble:
+            continue
+
+        found = False
+        for j in range(preamble):
+            for k in range(preamble):
+                if j == k:
+                    continue
+                if entries[i] == entries[i - j - 1] + entries[i - k - 1]:
+                    found = True
+                if found:
+                    break
+            if found:
+                break
+        if not found:
+            print(entries[i])
+            return entries[i]
+
+
+def puzzle2():
+    entries = [int(i) for i in DATA.split('\n') if i]
+
+    target = puzzle1()
+    start, end = 0, 1
+    while True:
+        s = sum(entries[start:end + 1])
+        if s < target:
+            end += 1
+        elif s > target:
+            start += 1
+            if start == end:
+                end += 1
+        else:
+            result = min(entries[start:end + 1]) + max(entries[start:end + 1])
+            print(result)
+            break
+
+
+if __name__ == '__main__':
+    try:
+        puzzle2()
+    except NameError as e:
+        if str(e) != "name 'puzzle2' is not defined":
+            raise
+        puzzle1()
